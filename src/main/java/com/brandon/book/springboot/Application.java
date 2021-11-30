d9b15617ef978573dedb2748072abca0a04782f7
brandon <dmdjqn@gmail.com>
2021-12-01 00:23:37 +0900

    기본 Controller 생성

---
+++ src/main/java/com/brandon/book/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.brandon.book.springboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
