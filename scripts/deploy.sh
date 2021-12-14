c507bfd0525cdc770d35f9d22c0fea66f531589d
brandon <dmdjqn@gmail.com>
2021-12-15 00:42:17 +0900

    자동배포스크립트수정 (grep jar -> grep java)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ echo ">build 파일복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo ">현재구동중인 애플리케이션 pid확인"
-CURRENT_PID=$(pgrep -fl basic-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl basic-webservice | grep java | awk '{print $1}')
 
 echo "현재구동중인 애플리케이션 pid : $CURRENT_PID"
 
