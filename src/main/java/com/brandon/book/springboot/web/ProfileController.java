76b92bf495d95b612028258e22808a4ffefa8c4c
brandon <dmdjqn@gmail.com>
2021-12-16 23:05:13 +0900

    profile 조회 API (nginx 적용)

--- src/main/java/com/brandon/book/springboot/web/ProfileController.java
+++ src/main/java/com/brandon/book/springboot/web/ProfileController.java
@@ -1,6 +1,8 @@
 package com.brandon.book.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +18,12 @@ public class ProfileController {
     @GetMapping("/profile")
     public String profile() {
 
-        List<String> profiles = Arrays.asList(environment.getActiveProfiles());
-        List<String> realProfiles = Arrays.asList("real1", "real2", "real3");
-        String defaultProfiles = profiles.isEmpty() ? "default" : profiles.get(0);
 
+        List<String> profiles = Arrays.asList(environment.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+
+        String defaultProfiles = profiles.isEmpty() ? "default" : profiles.get(0);
+        System.out.println("==>"+realProfiles.contains("oauth"));
         return profiles.stream()
                 .filter(p -> realProfiles.contains(p))
                 .findAny()
