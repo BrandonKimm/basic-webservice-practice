920447d3d23647690e8e2ccfe9fa385dc3e5625b
brandon <dmdjqn@gmail.com>
2021-12-18 13:32:30 +0900

    배포테스트

--- scripts/start.sh
+++ scripts/start.sh
@@ -36,7 +36,7 @@ IDLE_PROFILE=$(find_idle_profile)
 echo ">$JAR_NAME 실행 "
 
 nohup java -jar \
-        -Dspring.config.location=classpath:\application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+        -Dspring.config.location=classpath:\application.properties,classpath:/application-real2.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
         -Dspring.profiles.active=$IDLE_PROFILE \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
